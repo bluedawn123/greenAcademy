8563d3d3fad43fad7e04e252bdfac40d55e008e8
bluedawn123 <haemilyjh@naver.com>
2024-07-30 12:15:15 +0900

    카루셀 슬라이드 만들어보기

--- javascript/C/05_slideshow/js/script.js
+++ javascript/C/05_slideshow/js/script.js
@@ -1,6 +1,41 @@
 // 변수 지정
+let slideWrapper = document.querySelector('.slide-wrapper');
+let slides = document.querySelectorAll('.slide-container li');
+let slideCount = slides.length  //5
+let currentIdx = 0;
+let autoFade;
+//열자마자 첫 슬라이드 일단 보이게.
+slides[0].classList.add('active');
+
+/*
+한번체크
+let 대상 = setTimeout(할일, 시간);  clearTimeout(대상)
+
+일정시간마다 할일
+let 대상 = setInterval(할일, 시간); clearInterval(대상)
+*/
+
+function autoFadeStart(){
+    autoFade = setInterval(()=>{
+        let nextIdx = (currentIdx+1) % slideCount; //1,2,3,4,0,1,2...
+    
+        slides[currentIdx].classList.remove('active');
+        slides[nextIdx].classList.add('active');
+    
+        currentIdx = nextIdx;
+    }, 4000)
+}
+
+autoFadeStart();
 
 
+//마우스 들어왔을때 슬라이드 멈춤
+slideWrapper.addEventListener('mouseenter', () => {
+    clearInterval(autoFade);
+})
+slideWrapper.addEventListener('mouseleave', () => {
+    autoFadeStart();
+})
 // 슬라이드가 있으면 가로로 배열하기, 페이저 생성하기
 
 
---
+++ javascript/C/06_slideshow/js/script.js
@@ -0,0 +1,53 @@
+// 변수 지정
+let slideWrapper = document.querySelector('.slide-wrapper');
+let slideContainer = document.querySelector('.slide-container');
+let slides = document.querySelectorAll('.slide-container li');
+let slideCount = slides.length  //5
+let currentIdx = 0;
+let nextBtn = document.querySelector('#next');
+let prevBtn = document.querySelector('#prev');
+
+//처음실행
+goToslide(0)
+
+//슬라이드 배치
+// for(let i=0; i < slides.length; i++){
+//     slides[i].style.left = `${i *100}%`;
+// }
+slides.forEach( (item, index, all)=>{
+    item.style.left = `${index * 100}%`;
+})
+
+/*슬라이드 이동함수
+goToslide 함수는 매개변수 num으로 숫자가 들어오면 슬라이드를 이동시킨다. 
+goToslide(0)이면, slideContainer left 0
+goToslide(1)이면, slideContainer left -100%
+*/
+function goToslide(num){
+    slideContainer.style.left = `${num * -100}%`; //-100 -200 -300,,,
+    currentIdx = num;
+
+    //마지막이면 다음 버튼 안보이게
+    if(currentIdx === slides.length - 1){    
+        nextBtn.classList.add('disabled');
+    }else{
+        nextBtn.classList.remove('disabled');
+    }   
+
+    //처음이면 첫 버튼 안보이게
+    if(currentIdx === 0){    
+        prevBtn.classList.add('disabled');
+    }else{
+        prevBtn.classList.remove('disabled');
+    }   
+}
+
+
+//nextBtn 버튼을 클릭하면 현재 슬라이드 번호에 1을 더해서 gotoSilde에 전달한다. 
+nextBtn.addEventListener('click', ()=>{
+    goToslide(currentIdx + 1);  //-100 -200 -300...
+}); 
+
+prevBtn.addEventListener('click', ()=>{
+    goToslide(currentIdx - 1);  //역시 -100 -200 -300... 
+});
