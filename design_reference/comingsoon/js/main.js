47c0e68573f84d7e09e583a6daf17e5c56a25250
bluedawn123 <haemilyjh@naver.com>
2024-08-01 17:34:21 +0900

    디자인 참조코딩 커밍순(그래프,숫자변화)

---
+++ design_reference/comingsoon/js/main.js
@@ -0,0 +1,59 @@
+let btt = document.querySelector(".back-to-top");
+/*윈도우에 스크롤 생기면 할일
+스크롤 양이 300보다 크면 btt가 생기고 아니면 사라지게 + btt 클릭하면 화면상단으로 부드럽게
+*/
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    btt.classList.add("active");
+  } else {
+    btt.classList.remove("active");
+  }
+});
+btt.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+// progress Bar
+
+let progressBox = document.querySelector(".progress-box");
+let progressBar = document.querySelector(".progress-bar");
+let progressSpan = document.querySelector(".content span");
+let progressBoxOST = progressBox.offsetTop - 400; //progressBox가 화면상단에 떨어진 거리
+let one = document.querySelector(".progress-box");
+let progressRate = Number(progressBox.dataset.rate);
+let interval = 50;
+/*스크롤양이 progressBoxOST보다 많으면
+progressRate의 숫자만큼 숫자가 올라고고 + progressRate의 숫자만큼 너비가 넓어지도록
+*/
+
+let count = 0;
+let excuted = false;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > progressBoxOST) {
+    if (excuted == false) {
+
+      progressBar.style.width = progressRate + '%'; //여기로
+      progressBar.style.transition = progressRate * (interval/1000) + 's'; 
+
+
+      let timer = setInterval(() => {
+        count++;
+        progressSpan.innerText = count;
+        // progressBar.style.width = count + '%'; 위로 뺌.
+
+        if (count === progressRate) {
+          clearInterval(timer);
+        }
+      }, interval);
+
+      excuted = true;
+    }
+  }
+});
