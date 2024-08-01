87eb27e726aa8b38f76f6e456665977b1dc37018
bluedawn123 <haemilyjh@naver.com>
2024-08-01 11:32:24 +0900

    기본적인 html 공부

--- javascript/C/04_back_to_top/js/main.js
+++ javascript/C/04_back_to_top/js/main.js
@@ -1,39 +1,27 @@
 //아래를 쓰기 싫으면, html의 script를 바디 아래로
-document.addEventListener('DOMContentLoaded', () => {
-
-
-const goTop = document.querySelector('#go-top');
-//scroll은 window에 생김
-window.addEventListener('scroll', () =>{
+document.addEventListener("DOMContentLoaded", () => {
+  const goTop = document.querySelector("#go-top");
+  //scroll은 window에 생김
+  window.addEventListener("scroll", () => {
     let scrollAmt = window.scrollY;
-    
-    console.log(scrollAmt)
+
+    console.log(scrollAmt);
 
     //스크롤량이 300보다 많으면 나타나고 아니면 사라지게 해보자.
-    if(scrollAmt > 300){    
-        goTop.classList.add('active');
-    }else{
-        goTop.classList.remove('active');
+    if (scrollAmt > 300) {
+      goTop.classList.add("active");
+    } else {
+      goTop.classList.remove("active");
     }
-})
+  });
 
-goTop.addEventListener('click', (e)=>{
+  goTop.addEventListener("click", (e) => {
     e.preventDefault();
 
-    window.scrollBy(
-        {
-            left :0, top:0,
-            behavior:'smooth'
-        }
-    );
-})
-
-
-
-
-
-
-
-
-
+    window.scrollTo({
+      left: 0,
+      top: 0,
+      behavior: "smooth",
+    });
+  });
 });
