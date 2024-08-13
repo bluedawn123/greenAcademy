f8eadc70cca75aebb699779a4582003bd84d59ee
윤준호 <haemilyjh@naver.com>
2024-08-13 18:01:37 +0900

    svg로 그리기 최종

--- animations/svg_animation3/js/main.js
+++ animations/svg_animation3/js/main.js
@@ -1,6 +1,14 @@
-$(function(){
+const notepads = document.querySelector('.notepads');
 
-	
+/*
+윈도우에 스크롤이 생기면 할일
+  스크롤양이 300보다 크면 notepads에 active 추가
+  아니라면 active 제거
+*/
+window.addEventListener('scroll',()=>{
+  if(window.scrollY > 300){
+    notepads.classList.add('active');
+  }else{
+    notepads.classList.remove('active');
+  }
 });
-
-
